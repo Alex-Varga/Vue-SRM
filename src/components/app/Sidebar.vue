cd5ba6bded6efe093506a8738eb2f1f43a047339
Alex Varga <alexvarga09072001@gmail.com>
2021-10-06 16:24:30 +0300

    first commit

---
+++ src/components/app/Sidebar.vue
@@ -0,0 +1,39 @@
+<template>
+  <ul class="sidenav app-sidenav" :class="{open: isOpen}">
+
+    <router-link
+      v-for="link in links"
+      :key="link.url"
+      tag="li"
+      active-class="active"
+      :to="link.url"
+      :exact="link.exact"
+    >
+      <a href="#" class="waves-effect waves-orange pointer">{{link.title}}</a>
+    </router-link>
+  </ul>
+</template>
+
+<script>
+import localize from '../../filters/localize.filter'
+
+export default {
+  props: {
+    isOpen: {
+      type: Boolean
+    }
+  },
+  data: () => ({
+    links: [
+      {title: localize('Menu_Bill'), url: '/', exact: true},
+      {title: localize('Menu_History'), url: '/history'},
+      {title: localize('Menu_Planning'), url: '/planning'},
+      {title: localize('Menu_Record'), url: '/record'},
+      {title: localize('Menu_Categories'), url: '/categories'}
+    ]
+  })
+}
+</script>
+
+
+
--- src/main.js
+++ src/main.js
@@ -1,13 +1,50 @@
 import Vue from 'vue'
 import App from './App.vue'
-import './registerServiceWorker'
+import Vuelidate from 'vuelidate'
 import router from './router'
 import store from './store'
+import dateFilter from '@/filters/date.filter'
+import currencyFilter from './filters/currency.filter'
+import localizeFilter from './filters/localize.filter'
+import messagePlugin from '@/utils/message.plugin'
+import Paginate from 'vuejs-paginate'
+import Loader from '@/components/app/Loader.vue'
+import tooltipDirective from './directives/tooltip.directive'
+import './registerServiceWorker'
+import 'materialize-css/dist/js/materialize.min'
+
+import firebase from 'firebase';
+import 'firebase/auth';
+import 'firebase/firestore';
 
 Vue.config.productionTip = false
 
-new Vue({
-  router,
-  store,
-  render: h => h(App)
-}).$mount('#app')
+Vue.use(messagePlugin)
+Vue.use(Vuelidate)
+Vue.component('Loader', Loader)
+Vue.component('Paginate', Paginate)
+Vue.directive('tooltip', tooltipDirective)
+Vue.filter('date', dateFilter)
+Vue.filter('currency', currencyFilter)
+Vue.filter('localize', localizeFilter)
+
+firebase.initializeApp({
+  apiKey: "AIzaSyDFiAipkw15fheXygcN4K1_hbBiMIQ4dSg",
+  authDomain: "vue-srm-money.firebaseapp.com",
+  databaseURL: "https://vue-srm-money-default-rtdb.firebaseio.com",
+  projectId: "vue-srm-money",
+  storageBucket: "vue-srm-money.appspot.com",
+  messagingSenderId: "511632786373"
+})
+
+let app;
+
+firebase.auth().onAuthStateChanged(() => {
+  if (!app) {
+    app = new Vue({
+      router,
+      store,
+      render: h => h(App)
+    }).$mount('#app')
+  }
+})
